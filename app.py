02aa66fbbf257eb0dabcb30c5b6deef0223e888e
cknisaac <isaacchankn1@gmail.com>
2025-07-30 10:50:01 +0800

    init new files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+import pytest
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(port=os.environ.get('PORT', 5000), host="0.0.0.0", debug=True)
